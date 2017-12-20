d8d13cf9b3d1b5bb93d208841540a5858e2f2f0e
igagansingh <igagandeep.s@gmail.com>
2017-12-21 04:04:41 +0530

    Added all the requirements as suggested by the reviewer

--- curriculum_3/Log Analysis/README.md
+++ curriculum_3/Log Analysis/README.md
@@ -1,26 +1,51 @@
 # Logs Analysis
 
-Python file to test the understanding of database(s) basics and writing solution as per the specifications.
+Python file that answers few questions about a news database. Database here is a PostgreSQL, it consist of 3 tables.
+	Articles - Contains information about a article and who has written it (ID of author).
+	Authors	 - Contains information about a author, each author has an ID(unique).
+	Log 	 - Contains a log of requests made to this website (and articles), request status and other details.
+
+## Requirements
+
+This project uses Linux-based virtual machine 
+
+1. VirtualBox (https://www.virtualbox.org/wiki/Download_Old_Builds_5_1)
+2. Vagrant (https://www.vagrantup.com/downloads.html)
+3. News database(https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
 
 ## Setting up the environment (views)
 
-This file uses two views based on news database
-1. popularity (used in query1 and query2)
-2. perror_view (used in query3)
-### 1. popularity
+1. Download the VM configuration(https://github.com/udacity/fullstack-nanodegree-vm)
+2. Once the configuration is up navigate to vagrant folder, and start the virtual machine using:
+	> vagrant up (This will cause Vagrant to download the Linux operating system and install it.)
+	> vagrant ssh (To log in to your newly installed Linux VM)
 
-```
-create view popularity as select articles.title, articles.author, count(log.path) as views from articles join log on log.path like concat('%', articles.slug) group by articles.title, articles.author order by views desc;
-```
+3. You need to create two views for the file to run successfully
+	
+	##### popularity (used in query1 and query2)
 
-### 2. perror_view
+	```sql
+	CREATE VIEW popularity AS
+    SELECT articles.title, articles.author,	count(log.path) as views
+    FROM articles JOIN log on log.path = '/article/' || articles.slug
+    GROUP BY articles.title, articles.author
+    ORDER BY views DESC;
+	```
 
-```
-create view perror_view as select date(time), round(100.0*sum(case when log.status like '200 OK' then 0 else 1 end)/count(log.status),2) as perror from log group by date(time) order by perror desc;
-```
+	##### perror_view (used in query3)
+
+	```sql
+	CREATE VIEW perror_view AS
+    SELECT date(time), round(100.0*sum(case when log.status like '200 OK'
+    then 0 else 1 end)/count(log.status),2) as perror
+    FROM log
+    GROUP BY date(time)
+    ORDER BY perror DESC;
+	```
+
 ## Running the python file
 
-Open the terminal, navigate to the folder containing 'log_analysis.py' file and give python command to run this  file.
+After succesfully setting the virtual environment, database and views, open the terminal, navigate to the folder containing 'log_analysis.py' file and give python command to run this  file.
 
 ```
 $ python log_analysis.py
--- curriculum_3/Log Analysis/log_analysis.py
+++ curriculum_3/Log Analysis/log_analysis.py
@@ -1,56 +1,106 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
 """
 views used
 
 1.  Popularity (used in query1 and query2)
->   create view popularity as select articles.title, articles.author, count(log.path) as views from articles join log on log.path like concat('%', articles.slug) group by articles.title, articles.author order by views desc;  # noqa
+>   CREATE VIEW popularity AS
+    SELECT articles.title, articles.author, count(log.path) as views
+    FROM articles JOIN log on log.path = '/article/' || articles.slug
+    GROUP BY articles.title, articles.author
+    ORDER BY views DESC;
 
 2.  Error (used in query3)
->   create view perror_view as select date(time), round(100.0*sum(case when log.status like '200 OK' then 0 else 1 end)/count(log.status),2) as perror from log group by date(time) order by perror desc;  # noqa
-
+>   CREATE VIEW perror_view AS
+    SELECT date(time), round(100.0*sum(case when log.status like '200 OK'
+    then 0 else 1 end)/count(log.status),2) as perror
+    FROM log
+    GROUP BY date(time)
+    ORDER BY perror DESC;
 """
-DB = "news"
+DBNAME = "news"
 
-query1 = "select title, views from popularity limit 3"
-query2 = "select authors.name, sum(views) as views from popularity,authors where author=authors.id group by authors.name order by views desc"  # noqa
-query3 = "select * from perror_view where perror > 1"
 
-db = psycopg2.connect(database=DB)
-c = db.cursor()
+def db_connect():
+    """ Creates and returns a connection to the database defined by DBNAME,
+        as well as a cursor for the database.
 
-# Query 1
-print("\n1. What are the most popular three articles of all time?\n")
+        Returns:
+            db, c - a tuple. The first element is a connection to the database.
+                    The second element is a cursor for the database.
+    """
 
-c.execute(query1)
-result = c.fetchall()
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    return db, c
 
-print("Title of article\t\t\t\tViews")
-print("------------------------------------------------------------")
-for title, views in result:
-    print("{}\t\t{}".format(title, views))
 
-# Query 2
-print("\n2. Who are the most popular article authors of all time?\n")
+def execute_query(query):
+    """execute_query takes an SQL query as a parameter.
+        Executes the query and returns the results as a list of tuples.
+        args:
+            query - an SQL query statement to be executed.
+        returns:
+            A list of tuples containing the results of the query.
+    """
 
-c.execute(query2)
-result = c.fetchall()
+    db, c = db_connect()
+    c.execute(query)
+    result = c.fetchall()
+    db.close()
+    return result
 
-print("Author Name\t\t\tViews")
-print("------------------------------------------------------------")
-for author, views in result:
-    print("{}\t\t\t{}".format(author, views))
 
-# Query 3
-print("\n3. On which days did more than 1% of requests lead to errors?\n")
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
 
-c.execute(query3)
-result = c.fetchall()
+    query = '''SELECT title, views
+            FROM popularity
+            LIMIT 3'''
+    result = execute_query(query)
+    print('{0:<30}{1:^13}'.format('Title of article', 'Views'))
+    print("-" * 50)
+    for title, views in result:
+        print("{0:<30}{1:>8} views".format(title, views))
 
-print("Date\t\t\tPercentage Error")
-print("------------------------------------------------------------")
 
-for date, perror in result:
-    print("{}\t\t\t{}".format(date, perror))
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
 
-db.close()
+    query = '''SELECT authors.name, sum(views) AS views
+            FROM popularity, authors
+            WHERE author=authors.id
+            GROUP BY authors.name
+            ORDER BY views DESC'''
+    result = execute_query(query)
+    print('{0:<30}{1:^13}'.format('Author Name', 'Views'))
+    print('-' * 50)
+    for author, views in result:
+        print('{0:<30}{1:>8} views'.format(author, views))
+
+
+def print_errors_over_one():
+    """Prints out the days where more than 1% of logged access
+        requests were errors."""
+
+    query = '''SELECT *
+            FROM perror_view
+            WHERE perror > 1'''
+    result = execute_query(query)
+    print('{0:<24}{1:^13}'.format('Date', 'Percentage Error'))
+    print("-" * 50)
+    for date, perror in result:
+        print("{:%B %d, %Y}\t\t\t{} %".format(date, perror))
+
+if __name__ == '__main__':
+    # Query 1
+    print("\n1. What are the most popular three articles of all time?\n")
+    print_top_articles()
+    # Query 2
+    print("\n2. Who are the most popular article authors of all time?\n")
+    print_top_authors()
+    # Query 3
+    print("\n3. On which days did more than 1% of requests lead to errors?\n")
+    print_errors_over_one()
