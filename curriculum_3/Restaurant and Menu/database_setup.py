1e18c28949a5ae85db12490b89b9b8ff6dafdbb2
igagansingh <igagandeep.s@gmail.com>
2018-01-19 22:52:17 +0530

    Completed OAuth chapter, integrated google sign in. Updated database and content(lotsofmenu.py) for local permission system.

--- curriculum_3/Restaurant and Menu/database_setup.py
+++ curriculum_3/Restaurant and Menu/database_setup.py
@@ -6,14 +6,34 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+  __tablename__ = 'user'
+
+  id = Column(Integer, primary_key=True)
+  name =Column(String(80), nullable = False)
+  email = Column(String(250), nullable = False)
+  picture = Column(String(250))
+
+  @property
+  def serialize(self):
+	# Returns object data in easily serializable format
+	return{
+		'id' : self.id,
+		'name' : self.name,
+		'email' : self.email,
+		'picture' : self.picture
+	}
+
 class Restaurant(Base):
 	__tablename__ = 'restaurant'
 	name = Column(String(80), nullable=False)
 	id = Column(Integer, primary_key=True)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
 	@property
 	def serialize(self):
-		return {'id': self.id, 'name': self.name}
+		return {'id': self.id, 'name': self.name, 'user_id': self.user_id}
 
 
 class MenuItem(Base):
@@ -25,9 +45,12 @@ class MenuItem(Base):
 	price = Column(String(8))
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
 	@property
 	def serialize(self):
+		# Returns object data in easily serializable format
 		return {
 			'id' : self.id,
 			'name' : self.name,
@@ -36,5 +59,5 @@ class MenuItem(Base):
 			'price' : self.price
 		}
 
-engine = create_engine('sqlite:///resturantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 Base.metadata.create_all(engine)
